c74f785f17791289a1d5da899ec8b0096787d81a
Jaeook Jeong <wodnr0710@naver.com>
2022-08-05 01:54:22 +0900

    Create 루시와 엘라 찾기.sql

---
+++ MySQL/String, Date/루시와 엘라 찾기.sql
@@ -0,0 +1,3 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/59046
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE FROM ANIMAL_INS
+WHERE NAME IN ("Lucy","Ella","Pickle","Rogan","Sabrina","Mitty")
