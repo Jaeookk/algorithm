afa5985be4f8d99302771daa33a8d520068ca920
Jaeook Jeong <wodnr0710@naver.com>
2022-09-27 18:12:43 +0900

    사고과정 1. 전형적인 그리디 알고리즘 2. 가능한 회의 중에서 가장 먼저 끝나는 회의를 그리디하게 선택   2.1 선택 가능한 회의 중에서 가장 먼저 끝나는 회의를 선택해야 다음 회의가 시작하는 시간을 최소로 보장할 수 있기 때문 3. 이 방법이 맞는기 검증 (귀류법)   3.1 현재 시간이 t라고 할 때 시작 시간이 t 이상인 모든 회의 중에서 가장 먼저 끝나는 회의를 택하는 것이 최적해이다.   3.2 명제를 거짓이라고 가정하면 회의A(가장 먼저 끝나는 회의) 말고 다른 회의(B)를 선택 했을 때 더 많은 회의를 배정할 수 있는 경우의 수가 존재한다.   3.3 그러면 회으 V로 이어지는 스케쥴을 생각할 수 있는데, 혀기서 회의 B 대신 회의 A를 선택하면 어떤 일이 생길지 생각해본다.   3.4 회의 B를 회의 A로 변경해도 스케쥴에는 아무런 문제가 없다. 회의 A는 남아있는 가능한 회의 중에서 가장 먼저 끝나는 회의니까 당연히 회의 A가 회의 B보다 먼저 끝나기 때문git commit -m 사고과정

---
+++ 그리디/회의실 배정.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+data = []
+for i in range(N):
+    a, b = map(int, input().split())
+    data.append((a, b))
+data.sort(key=lambda x: (x[1], x[0]))
+# 끝나는 시간이 같다면, 시작하는 시간을 빠른 순으로 정렬을 하는 이유
+# 만약에 정렬이 시작시간에 전혀 관계없이, 종료시간으로만 정렬되게 된다면, 문제가 생기게 됩니다.
+
+# 1 3
+# 8 8
+# 4 8
+
+# 이라는 예시에 대해서, 종료시간으로만 정렬하면, 위와 같이 정렬될 수도 있습니다.
+# 그럴 때, 1 3과 8 8만 세므로 2가 나와서 오답이 됩니다.
+
+# 실제로는 1~3시 1번, 4~8시 2번, 8~8시 3번으로 총 3개가 나와야합니다
+
+result = []
+result.append(data[0])
+
+for i in range(1, len(data)):
+    if data[i][0] >= result[-1][1]:
+        result.append(data[i])
+print(len(result))
