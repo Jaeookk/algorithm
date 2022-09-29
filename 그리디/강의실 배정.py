0e1ef5fd7aa2fa09deec98c43b94a60ce694ac02
Jaeook Jeong <wodnr0710@naver.com>
2022-09-29 17:47:53 +0900

    백준 11000 / 최소힙

--- 그리디/강의실 배정.py
+++ 그리디/강의실 배정.py
@@ -10,15 +10,17 @@ for _ in range(N):
     time.append((s, t))
 time = sorted(time, key=lambda x: (x[0], x[1]))
 
-# 최소힙에 가장 빠르게 시작하고 가장 빠르게 끝나는 수업의 종료시간 넣기
+# 최소힙에 첫 강의실의 종료시간 넣기
 heapq.heappush(h, time[0][1])
 
 for i in range(1, N):
-    # 다음 수업의 시작 시간이 이전 수업의 종료시간보다 작으면
+    # 다음 수업의 시작 시간이 이전 수업의 가장 작은 종료시간보다 작으면
+    # 강의실 하나 더 필요하므로, 힙에 새로운 강의실의 종료시간을 추가.
     if time[i][0] < h[0]:
         heapq.heappush(h, time[i][1])
-    # 다음 수업의 시작 시간이 이전 수업의 종료시간보다 크다면
+    # 다음 수업의 시작 시간이 이전 수업의 가장 작은 종료시간보다 크다면
     else:
+        # 가장 빨리 수업이 끝나는 강의실의 종료시간 최신화.
         heapq.heappop(h)
         heapq.heappush(h, time[i][1])
 print(len(h))
