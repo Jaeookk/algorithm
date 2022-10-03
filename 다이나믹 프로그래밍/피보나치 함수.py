2ccf142012d3d577f4d4a9d1b72442b8cf059164
Jaeook Jeong <wodnr0710@naver.com>
2022-10-03 20:00:11 +0900

    백준 1003

---
+++ 다이나믹 프로그래밍/피보나치 함수.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+
+def fibonacci(num):
+    length = len(zero)
+    if num >= length:
+        for i in range(length, num + 1):
+            zero.append(zero[i - 1] + zero[i - 2])
+            one.append(one[i - 1] + one[i - 2])
+    print(zero[num], one[num])
+
+
+for _ in range(T):
+    fibonacci(int(input()))
