2ce8ebffb527073a3566bf72aec2b873736a336b
Jaeook Jeong <wodnr0710@naver.com>
2022-10-04 17:22:34 +0900

    백준 2579 / 2차원 배열 dp 테이블 풀이

---
+++ 다이나믹 프로그래밍/계단 오르기.py
@@ -0,0 +1,36 @@
+# 1. 테이블 정의하기
+# d[i][j] = 현재까지 j개의 계단을 연속해서 밟고, i번째 계단까지 올라섰을 때 점수 합의 최댓값,
+# 단 i번째 계단은 반드시 밟아야함. -> j는 1 or 2
+# 2. 점화식 찾기
+# d[k][1] = k번째 계단을 밟았을때 연속되는 첫번째 계단이므로
+#           k-2번째 계단을 밟고 2계단 건너뛰어 k번째 계단을 밟았다는 의미
+# 즉, d[k][1] = max(d[k-2][1], d[k-2][2]) + s[k] (s[k]는 k번째 계단의 점수)
+
+# d[k][2] = d[k-1][1] + s[k]
+# 3. 초기값 정하기
+# d[1][1] = s[1], d[1][2] = 0
+# d[2][1] = s[2], d[2][2] = s[1] + s[2]
+
+n = int(input())
+scores = [0] + [int(input()) for _ in range(n)]
+d = [[0] * 3 for _ in range(n + 1)]
+
+
+def main():
+    if n == 1:
+        print(scores[1])
+        return
+    d[1][1] = scores[1]
+    d[1][2] = 0
+    d[2][1] = scores[2]
+    d[2][2] = scores[1] + scores[2]
+
+    for i in range(3, n + 1):
+        d[i][1] = max(d[i - 2][1], d[i - 2][2]) + scores[i]
+        d[i][2] = d[i - 1][1] + scores[i]
+
+    print(max(d[n]))
+
+
+if __name__ == "__main__":
+    main()
