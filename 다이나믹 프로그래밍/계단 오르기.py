353fb6508ebb1a990e6e4c060da3df4520a38aa5
Jaeook Jeong <wodnr0710@naver.com>
2022-10-04 17:27:24 +0900

    백준 2579 / 풀이법 2개

--- 다이나믹 프로그래밍/계단 오르기.py
+++ 다이나믹 프로그래밍/계단 오르기.py
@@ -32,5 +32,22 @@ def main():
     print(max(d[n]))
 
 
+# # 1차원 dp 풀이 방법
+# N = int(input())
+# stair = [0] * 301
+# for i in range(N):
+#     stair[i] = int(input())
+
+# def main():
+#     DP = [0] * 301
+#     DP[0] = stair[0]
+#     DP[1] = stair[0] + stair[1]
+#     DP[2] = max(stair[0] + stair[2], stair[1] + stair[2])
+
+#     for i in range(3, N):
+#         DP[i] = max(DP[i - 3] + stair[i - 1] + stair[i], DP[i - 2] + stair[i])
+
+#     print(DP[N - 1])
+
 if __name__ == "__main__":
     main()
