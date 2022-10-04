9d9fbc5a64cef18dba9b7ed10750e18cc1bbbc7f
Jaeook Jeong <wodnr0710@naver.com>
2022-10-04 18:55:42 +0900

    백준 12852

---
+++ 다이나믹 프로그래밍/1로 만들기 2.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+d = [0] * (n + 1)
+history = [i for i in range(n + 1)]
+history[1] = 0
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + 1
+    history[i] = i - 1
+
+    if i % 3 == 0 and d[i] > d[i // 3] + 1:
+        d[i] = d[i // 3] + 1
+        history[i] = i // 3
+    if i % 2 == 0 and d[i] > d[i // 2] + 1:
+        d[i] = d[i // 2] + 1
+        history[i] = i // 2
+
+print(d[n])
+print(n, end=" ")
+while True:
+    if history[n] == 0:
+        break
+    print(history[n], end=" ")
+    n = history[n]
