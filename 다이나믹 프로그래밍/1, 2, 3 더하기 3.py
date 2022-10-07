a8556a65204721003f5da9d9a3f9c61aa56c67f8
Jaeook Jeong <wodnr0710@naver.com>
2022-10-07 22:31:41 +0900

    백준 15988

--- 다이나믹 프로그래밍/1, 2, 3 더하기 3.py
+++ 다이나믹 프로그래밍/1, 2, 3 더하기 3.py
@@ -1,25 +1,34 @@
-# 1. 테이블 정의하기
-# d[i] = i를 1,2,3의 합으로 나타내는 방법의 수
-# 2. 점화식 찾기
-# d[4] = ?
-# 1+1+1+1, 3+1, 2+1+1, 1+2+1 (3을 1,2,3의 합으로 나타내는 방법)+1 -> d[3]
-# 1+1+2, 2+2 (2를 1,2,3의 합으로 나타내는 방법)+2 -> d[2]
-# 1+3 (1을 1,2,3의 합으로 나타내는 방법)+3 -> d[1]
-# ----d[i] = d[i-1]+d[i-2]+d[i-3]----
+# 시간초과
+# import sys
+
+# sys.setrecursionlimit(10000001)
+# n = int(sys.stdin.readline())
+# d = [0] * 10000001
+# d[1] = 1
+# d[2] = 2
+# d[3] = 4
+
+# for _ in range(n):
+#     x = int(input())
+#     if x < 4:
+#         print(d[x])
+#         continue
+#     for i in range(4, x + 1):
+#         d[i] = (d[i - 1] + d[i - 2] + d[i - 3]) % 1000000009
+#     print(d[x])
+
 import sys
 
-sys.setrecursionlimit(10000001)
-n = int(sys.stdin.readline())
-d = [0] * 10000001
-d[1] = 1
-d[2] = 2
-d[3] = 4
+input = sys.stdin.readline
 
-for _ in range(n):
-    x = int(input())
-    if x < 4:
-        print(d[x])
-        continue
-    for i in range(4, x + 1):
-        d[i] = (d[i - 1] + d[i - 2] + d[i - 3]) % 1000000009
-    print(d[x])
+dp = [0 for i in range(1000001)]
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+for i in range(3, 1000001):
+    dp[i] = dp[i - 1] % 1000000009 + dp[i - 2] % 1000000009 + dp[i - 3] % 1000000009
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    print(dp[n] % 1000000009)
