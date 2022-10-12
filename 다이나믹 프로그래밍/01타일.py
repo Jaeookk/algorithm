c88709297c4d30b6f11cbe4e073f1327242f3b8c
Jaeook Jeong <wodnr0710@naver.com>
2022-10-12 15:27:42 +0900

    백준 1904

---
+++ 다이나믹 프로그래밍/01타일.py
@@ -0,0 +1,13 @@
+# 점화식 dp[i] = dp[i-1] + dp[i-2]
+n = int(input())
+
+dp = [0] * (n + 1)
+dp[1] = 1
+dp[2] = 2
+
+if n < 3:
+    print(dp[n])
+else:
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+    print(dp[n])
