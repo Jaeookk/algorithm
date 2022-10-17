d47280b305bc947fbd0fea268f978af8239b0e0f
Jaeook Jeong <wodnr0710@naver.com>
2022-10-17 19:29:54 +0900

    BOJ 15665 / 중복 확인 시 시간초과 어려웠음..

---
+++ 백 트래킹/N과 M 11.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+
+arr = [0] * (m + 1)
+
+
+def dfs(k):
+    global arr
+
+    if k == m:
+        print(*arr[:-1])
+        return
+
+    temp = 0
+    for i in range(n):
+        if temp != data[i]:
+            arr[k] = data[i]
+            temp = data[i]
+            dfs(k + 1)
+
+
+dfs(0)
