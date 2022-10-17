7aa971ed3f83a6660c2f8f4f9c059702cb5993d4
Jaeook Jeong <wodnr0710@naver.com>
2022-10-17 19:28:19 +0900

    BOJ 15654

---
+++ 백 트래킹/N과 M 5.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+
+arr = [0] * 10
+visited = [False] * 10
+
+
+def dfs(k):
+    global arr, visited
+
+    if k == m:
+        for i in range(m):
+            print(arr[i], end=" ")
+        print()
+        return
+
+    for i in range(len(data)):
+        if not visited[i]:
+            arr[k] = data[i]
+            visited[i] = True
+            dfs(k + 1)
+            visited[i] = False
+
+
+dfs(0)
