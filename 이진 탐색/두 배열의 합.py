6064684828cb1ddfb862ac5301b15394033a7e72
Jaeook Jeong <wodnr0710@naver.com>
2022-11-05 20:58:13 +0900

    BOJ 2143 / bisect_left, right 직접 구현

--- 이진 탐색/두 배열의 합.py
+++ 이진 탐색/두 배열의 합.py
@@ -1,21 +1,22 @@
 def bisect_right(arr, target, start, end):
     while start < end:
         mid = (start + end) // 2
-        if arr[mid] > target:
+        if arr[mid] > target:  # mid 값이 target보다 크다면
+            # mid의 인덱스 값이 target이 들어가야할 자리일 수도 있다. 그러므로 end를 mid로 내린다.
             end = mid
-        else:
-            start = mid + 1
+        else:  # mid 값이 target보다 작거나 같다면
+            start = mid + 1  # start를 점점 올린다.
     return end
 
 
 def bisect_left(arr, target, start, end):
     while start < end:
         mid = (start + end) // 2
-        if arr[mid] < target:
+        if arr[mid] < target:  # mid 값이 target보다 작다면
             start = mid + 1
-        else:
-            end = mid
-    return start
+        else:  # mid 값이 target보다 크거나 같다면
+            end = mid  # end를 mid로 내리면서 가장 왼쪾의 target값으로 end를 옮긴다. 이후 start는 line 14,15에 의해 점점 end쪽으로 올라온다.
+    return start  # start와 end가 겹치면 start 반환.
 
 
 if __name__ == "__main__":
