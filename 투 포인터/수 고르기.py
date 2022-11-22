3aa36d95aef50842427b92b62b3d12565ef39821
Jaeook Jeong <wodnr0710@naver.com>
2022-11-22 20:30:15 +0900

    BOJ 2003

---
+++ 투 포인터/수 고르기.py
@@ -0,0 +1,17 @@
+import sys
+
+
+n, m = map(int, input().split())
+arr = [int(input()) for _ in range(n)]
+arr.sort()
+
+min_value = sys.maxsize
+end = 0
+for start in range(n):
+    while end < n and (arr[end] - arr[start] < m):
+        end += 1
+    if end == n:
+        break
+    min_value = min(min_value, arr[end] - arr[start])
+
+print(min_value)
