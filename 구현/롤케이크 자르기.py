2101fecca9e12bd847d52e1a16866f4932b31718
Jaeook Jeong <wodnr0710@naver.com>
2022-12-21 16:19:09 +0900

    Update 롤케이크 자르기.py
    이진탐색으로 풀 수 있을 것 같은데.... 흠...

--- 구현/롤케이크 자르기.py
+++ 구현/롤케이크 자르기.py
@@ -2,17 +2,19 @@
 from collections import Counter
 
 def solution(topping):
-    cheolsu=Counter(topping)
-    brother=set()
-    answer=0
+    cheolsu = Counter(topping)
+    brother = set()
+    answer = 0
+    
     for i in topping:
-        cheolsu[i]-=1 # 철수꺼에서 토핑 하나씩 빼기
+        cheolsu[i] -= 1 # 철수꺼에서 토핑 하나씩 빼기
         brother.add(i) # 뺀거를 동생에게 주기
         
-        if cheolsu[i]==0:  # 한 종류의 토핑이 모두 다 빠졌다면
+        if cheolsu[i] == 0:  # 한 종류의 토핑이 모두 다 빠졌다면
             cheolsu.pop(i) # 제거
             
-        if len(cheolsu)==len(brother): # 철수와 동생의 토핑 종류 개수가 같다면
-            answer+=1
+        if len(cheolsu) == len(brother): # 철수와 동생의 토핑 종류 개수가 같다면
+            answer += 1
     
     return answer
+
