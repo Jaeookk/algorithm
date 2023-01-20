d1a76b13884a7960f35c1f8656fe4543dc06e2e8
Jaeook Jeong <wodnr0710@naver.com>
2023-01-20 12:49:35 +0900

    BOJ 1874

---
+++ 스택 수열.py
@@ -0,0 +1,26 @@
+n = int(input())
+stack = []
+result = []
+count = 0
+X = True
+
+for _ in range(n):
+    num = int(input())
+
+    while count < num:
+        count += 1
+        stack.append(count)
+        result.append("+")
+
+    if stack[-1] == num:
+        stack.pop()
+        result.append("-")
+    else:
+        X = False
+        break
+
+if X == False:
+    print("NO")
+else:
+    for i in result:
+        print(i)
