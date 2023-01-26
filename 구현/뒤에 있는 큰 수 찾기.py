9522ed293107c2f2357afbc8c673be424517b97c
Jaeook Jeong <wodnr0710@naver.com>
2023-01-26 21:18:43 +0900

    Create 뒤에 있는 큰 수 찾기.py
    이중 포문을 사용해서 시간초과가 많이 났었다.

---
+++ 구현/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154539
+def solution(numbers):
+    answer = [-1] * len(numbers)
+    stack = []
+    
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+
+        stack.append(i)
+
+    return answer
