68769a81b5e2d6bf088fb2a68612889538ffcecc
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-19 22:24:54 +0900

    설명 추가

--- 당구 연습.py
+++ 당구 연습.py
@@ -1,6 +1,9 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/169198?language=python3
 def solution(m, n, startX, startY, balls):
     answer = []
+    # 선대칭과 점대칭을 이용
+    # 목표 공이 벽에 안 붙어 있는 경우 -> 최소 거리를 위한 시작 공 위치(-startX, startY) or (startX, -startY)
+    # 목표 공이 벽에 붙어 있는 경우 -> (startX + (m - startX) * 2, startY) or (startX, startY + (n - startY) * 2)
     targets = [(-startX, startY), (startX, 2 * n - startY), (2 * m - startX, startY), (startX, -startY)]
     for ball in balls:
         new_targets = [targets[0], targets[1], targets[2], targets[3]]
