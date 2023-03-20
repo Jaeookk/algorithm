cfe03688ef50dea5fe66db18eb5797b3ddd66bec
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-20 20:43:45 +0900

    programmers lv.3 등굣길.py

---
+++ 다이나믹 프로그래밍/등굣길.py
@@ -0,0 +1,18 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42898
+
+
+def solution(m, n, puddles):
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]  # dp 테이블 초기화
+    dp[1][1] = 1
+
+    for col, row in puddles:
+        dp[row][col] = -1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+            else:
+                dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+
+    return dp[-1][-1]
