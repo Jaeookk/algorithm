12cf5bc9de216ecefba49dc9eb37dc016d4b2826
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-26 00:45:35 +0900

    programmers lv.3

--- 그리디/단속카메라.py
+++ 그리디/단속카메라.py
@@ -26,13 +26,7 @@ def solution(routes):
 
 
 print(solution([[-2, -1], [1, 2], [-3, 0]]))  # 2
-print(
-    solution(
-        [
-            [0, 0],
-        ]
-    )
-)  # 1
+print(solution([[0, 0]]))  # 1
 print(solution([[0, 1], [0, 1], [1, 2]]))  # 1
 print(solution([[0, 1], [2, 3], [4, 5], [6, 7]]))  # 4
 print(solution([[-20, -15], [-14, -5], [-18, -13], [-5, -3]]))  # 2
