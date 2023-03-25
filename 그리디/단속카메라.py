57fa00dcaa3ca6f4c9db0a3adea5f9dc959fe712
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-26 00:44:54 +0900

    programmers lv.3

--- 그리디/단속카메라.py
+++ 그리디/단속카메라.py
@@ -1,13 +1,40 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42884
 
-# 가장 간단한 코드
+# 처음 틀린 코드
 # def solution(routes):
+#     answer = 0
 #     routes.sort(key=lambda x: x[0])
-#     a = 1
-#     m = 30_000
-#     for i, j in routes:
-#         if i > m:
-#             a += 1
-#             m = j
-#         m = min(m, j)
-#     return a
+#     camera = 30000
+#     for start, end in routes:
+#         if start >= camera:
+#             answer += 1
+#             camera = end
+#         camera = min(camera, end)
+#     return answer
+
+
+def solution(routes):
+    answer = 0
+    routes.sort(key=lambda x: x[0])
+    camera = 30000
+    for start, end in routes:
+        if start > camera:
+            answer += 1
+            camera = end
+        camera = min(camera, end)
+    return answer + 1
+
+
+print(solution([[-2, -1], [1, 2], [-3, 0]]))  # 2
+print(
+    solution(
+        [
+            [0, 0],
+        ]
+    )
+)  # 1
+print(solution([[0, 1], [0, 1], [1, 2]]))  # 1
+print(solution([[0, 1], [2, 3], [4, 5], [6, 7]]))  # 4
+print(solution([[-20, -15], [-14, -5], [-18, -13], [-5, -3]]))  # 2
+print(solution([[-20, 15], [-14, -5], [-18, -13], [-5, -3]]))  # 2
+print(solution([[-20, 15], [-20, -15], [-14, -5], [-18, -13], [-5, -3]]))  # 2
