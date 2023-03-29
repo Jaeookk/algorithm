154a63f856d8189edeea7980ce8166107aa6d35e
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-29 19:02:45 +0900

    boj 13458

--- 삼성 SW 역량 테스트 기출 문제/시험감독.py
+++ 삼성 SW 역량 테스트 기출 문제/시험감독.py
@@ -1,15 +1,5 @@
 n = int(input())
 a = list(map(int, input().split()))
 b, c = map(int, input().split())
-ret = 0
-
-for i in range(n):
-    if a[i] > b:
-        div, mod = divmod((a[i] - b), c)
-        if mod == 0:
-            ret += div + 1
-        else:
-            ret += div + 2
-    else:
-        ret += 1
+ret = sum((x - b - 1) // c + 2 if x > b else 1 for x in a)
 print(ret)
