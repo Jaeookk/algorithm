d4541a7133bf7f500b9c37ac1634f128d4a89c79
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-30 20:28:26 +0900

    boj 14499

---
+++ 삼성 SW 역량 테스트 기출 문제/주사위 굴리기.py
@@ -0,0 +1,33 @@
+n, m, x, y, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+directions = list(map(int, input().split()))  # 1:동 2:서 3:북 4:남
+
+cube = [[0] * 3 for _ in range(4)]
+
+
+def move(direction):
+    if direction == 3:
+        cube[0][1], cube[1][1], cube[2][1], cube[3][1] = cube[1][1], cube[2][1], cube[3][1], cube[0][1]
+    elif direction == 1:
+        cube[1][0], cube[1][1], cube[1][2], cube[3][1] = cube[3][1], cube[1][0], cube[1][1], cube[1][2]
+    elif direction == 2:
+        cube[3][1], cube[1][0], cube[1][1], cube[1][2] = cube[1][0], cube[1][1], cube[1][2], cube[3][1]
+    else:
+        cube[1][1], cube[2][1], cube[3][1], cube[0][1] = cube[0][1], cube[1][1], cube[2][1], cube[3][1]
+    return cube
+
+
+dx, dy = [0, 0, -1, 1], [1, -1, 0, 0]
+cube[3][1] = graph[x][y]
+
+for direction in directions:
+    nx, ny = x + dx[direction - 1], y + dy[direction - 1]
+    if 0 <= nx < n and 0 <= ny < m:
+        cube = move(direction)
+        if graph[nx][ny] == 0:
+            graph[nx][ny] = cube[3][1]
+        else:
+            cube[3][1], graph[nx][ny] = graph[nx][ny], 0
+        x, y = nx, ny
+        print(cube[1][1])
