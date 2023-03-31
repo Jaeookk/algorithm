6b60d32016a302c0fc7167ceb9ed7502ec7eb381
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-03-31 14:01:34 +0900

    refactoring code, boj 1926

--- DFS_BFS/그림.py
+++ DFS_BFS/그림.py
@@ -1,42 +1,37 @@
+import sys
 from collections import deque
 
-n, m = map(int, input().split())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+n, m = map(int, sys.stdin.readline().split())
+
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+visited = [[False for _ in range(m)] for _ in range(n)]
 
 
 def bfs(x, y, visited):
     q = deque()
     q.append((x, y))
-    visited[x][y] = True
-    area = 1
+    dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+    cnt = 0
     while q:
         x, y = q.popleft()
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx <= -1 or nx >= n or ny <= -1 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
-            if graph[nx][ny] == 1 and visited[nx][ny] == False:
-                q.append((nx, ny))
+            nx, ny = x + dx[i], y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and graph[nx][ny] == 1:
                 visited[nx][ny] = True
-                area += 1
-    return area
+                q.append((nx, ny))
+                cnt += 1
+    return cnt + 1
 
 
-visited = [[False] * m for _ in range(n)]
-result = 0
-count = 0
+count = []
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1 and visited[i][j] == False:
-            temp = bfs(i, j, visited)
-            result = max(result, temp)
-            count += 1
-print(count)
-print(result)
+        if graph[i][j] == 1 and not visited[i][j]:
+            visited[i][j] = True
+            count.append(bfs(i, j, visited))
+
+print(len(count))
+if count:
+    print(max(count))
+else:
+    print(0)
