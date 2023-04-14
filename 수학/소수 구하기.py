1294e1d458493b33acb634c5319205bbb44b6add
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-04-15 00:41:41 +0900

    boj 1929

--- 수학/소수 구하기.py
+++ 수학/소수 구하기.py
@@ -6,10 +6,9 @@ arr = [True] * (n + 1)
 arr[1] = False
 
 for i in range(1, int(math.sqrt(n)) + 1):
-    if arr[i] == False:
-        continue
-    for j in range(i**2, len(arr), i):
-        arr[j] = False
+    if arr[i] == True:
+        for j in range(i * 2, len(arr), i):
+            arr[j] = False
 
 for i in range(m, len(arr)):
     if arr[i] == True:
