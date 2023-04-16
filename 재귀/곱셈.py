c988ca22fa3fa5b2ed33201ad10683805b196735
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-04-16 15:41:31 +0900

    BOJ 1629 / 어려운데..?

--- 재귀/곱셈.py
+++ 재귀/곱셈.py
@@ -1,3 +1,4 @@
+# BOJ 1629
 a, b, c = map(int, input().split())
 
 
@@ -5,13 +6,17 @@ def pow(x, y, z):
     if y == 1:
         return x % z
     val = pow(x, y // 2, z)
-    val = val * val % z
-    if y % 2 == 0:
+    val = val * val % z  # a^b의 mod c가 val 이므로, a^(2b)의 mod c는 val ^ 2 % c
+    if y % 2 == 0:  # 지수가 짝수라면 그냥 내보내고
         return val
-    return val * x % z
+    return val * x % z  # 지수가 홀수라면 a^3(mod c) -> a^2(mod c) * a % c
 
 
 print(pow(a, b, c))
+# a^b ≡ k(mod c) -> a^(2b) ≡ k^2(mod c)
+# 12^58 ≡ 4(mod 67) -> 12^116 ≡ 16(mod 67)
+# 즉, a의 mod c를 구할 수 있다면 a^2b의 mod c도 구할 수 있다.
+#
 # 코드에서 POW 함수가 바로 a^b mod m을 계산해주는 함수이다.
 # base condition은 05번째 줄에 명시. b = 1일 때를 base condition으로 뒀는데 b = 0일 때로 두어도 된다.
 # 그리고 a가 m보다 클 수 있기 때문에 a를 반환하는 대신 a % m을 반환
