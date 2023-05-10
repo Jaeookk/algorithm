f01813604c936c38d6920af0b91eb1ea6a3e2c50
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-05-10 09:56:12 +0900

    boj 15658

---
+++ 백 트래킹/연산자 끼워넣기 (2).py
@@ -0,0 +1,31 @@
+# 백준 15658 실버2
+
+
+def dfs(i, value, add, sub, mul, div):
+    # 모든 연산자를 다 사용한 경우, 최솟값과 최댓값 업데이트
+    if i == n:
+        result["min_value"] = min(result["min_value"], value)
+        result["max_value"] = max(result["max_value"], value)
+
+    else:
+        # 각 연산자에 대하여 재귀적으로 수행
+        if add:
+            dfs(i + 1, value + data[i], add - 1, sub, mul, div)
+        if sub:
+            dfs(i + 1, value - data[i], add, sub - 1, mul, div)
+        if mul:
+            dfs(i + 1, value * data[i], add, sub, mul - 1, div)
+        if div:
+            dfs(i + 1, int(value / data[i]), add, sub, mul, div - 1)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    data = list(map(int, input().split()))
+    add, sub, mul, div = map(int, input().split())
+    result = {"max_value": -1e9, "min_value": 1e9}
+
+    dfs(1, data[0], add, sub, mul, div)
+
+    print(result["max_value"])
+    print(result["min_value"])
