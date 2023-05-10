c5d3877ad083c8b9e1a9718e1860b9d91a4c4b83
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-05-10 12:38:09 +0900

    boj 1987

---
+++ 백 트래킹/알파벳.py
@@ -0,0 +1,31 @@
+# 백준 1987 골드4 시간이 드럽다...
+
+import sys
+
+input = sys.stdin.readline
+
+r, c = map(int, input().split())
+maps = []
+for _ in range(r):
+    maps.append(list(input()))
+ans = 0
+alphas = set()
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def dfs(x, y, count):
+    global ans
+    ans = max(ans, count)
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < r and 0 <= ny < c and not maps[nx][ny] in alphas:
+            alphas.add(maps[nx][ny])
+            dfs(nx, ny, count + 1)
+            alphas.remove(maps[nx][ny])
+
+
+alphas.add(maps[0][0])
+dfs(0, 0, 1)
+print(ans)
