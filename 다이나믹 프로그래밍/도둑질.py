fa0480c24f4ab3b8ef6e8f3200cd459259c30eee
Jaeook Jeong(local) <wodnr0710@naver.com>
2023-09-08 23:59:28 +0900

    programmers LV.4

---
+++ 다이나믹 프로그래밍/도둑질.py
@@ -0,0 +1,14 @@
+def solution(money):
+    # 처음 집을 털었을 때 -> 마지막 집 털지 못함
+    dp1 = [0] * len(money)
+    dp1[0], dp1[1] = money[0], max(money[0], money[1])
+    for i in range(2, len(money) - 1):
+        dp1[i] = max(dp1[i - 1], dp1[i - 2] + money[i])
+
+    # 처음 집을 안 털었을 때 -> 마지막 집 털수 있음
+    dp2 = [0] * len(money)
+    dp2[0], dp2[1] = 0, money[1]
+    for i in range(2, len(money)):
+        dp2[i] = max(dp2[i - 1], dp2[i - 2] + money[i])
+
+    return max(max(dp1), max(dp2))
